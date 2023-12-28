fbc87fc556f76ec08f9b3b23f6e6d85ff1047c2c
Felipe Siqueira <feliperdsiqueira@gmail.com>
2023-12-28 00:40:59 -0300

    feat: formatando main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
- 
- backend "s3" {
-   region = "sa-east-1"
-   key    = "terraform.tfstate"
- }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region = "sa-east-1"
+    key    = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
@@ -16,20 +16,20 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "golang_example_function" {
-  filename         = "./lambda.zip"                       # caminho para o código da sua função
-  function_name    = "golang-example_function"          # nome da sua função Lambda
-  role             = aws_iam_role.lambda_role.arn       # ARN da role IAM associada à função
-  handler          = "main.handler"                     # o arquivo e a função que será executada (ex: nome_do_arquivo.função)
-  runtime          = "go1.x"                            # substitua pelo runtime adequado à sua função
-  memory_size      = 128                                # tamanho da memória alocada à função
-  timeout          = 30                                 # tempo limite de execução da função em segundos
+  filename      = "./lambda.zip"               # caminho para o código da sua função
+  function_name = "golang-example_function"    # nome da sua função Lambda
+  role          = aws_iam_role.lambda_role.arn # ARN da role IAM associada à função
+  handler       = "main.handler"               # o arquivo e a função que será executada (ex: nome_do_arquivo.função)
+  runtime       = "go1.x"                      # substitua pelo runtime adequado à sua função
+  memory_size   = 128                          # tamanho da memória alocada à função
+  timeout       = 30                           # tempo limite de execução da função em segundos
 
   source_code_hash = filebase64sha256("lambda.zip")
 }
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
